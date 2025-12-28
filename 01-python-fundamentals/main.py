0b134ab80ead6a2a69a1abc016cededd6d654b89
sreerajvarmar <sreerajv2k3@gmail.com>
2025-12-28 15:59:12 +0530

    Update main.py

--- 01-python-fundamentals/main.py
+++ 01-python-fundamentals/main.py
@@ -1,1 +1,37 @@
+def calculate_grade(marks):
+    if marks>=90:
+        return 'A+'
+    elif marks>=80:
+        return 'A'
+    elif marks>=70:
+        return 'B+'
+    elif marks>=60:
+        return 'B'
+    elif marks>50:
+        return 'C+'
+    elif marks==50:
+        return 'C'
+    else:
+        return 'Fail'
 
+num_students=int(input("Enter the Number of Students: "))
+
+students={}
+
+for i in range(num_students):
+    name=input("Enter Student Name: ")
+    marks=float(input("Enter Student Marks: "))
+    students[name]=marks
+
+total_marks=sum(students.values())
+average_marks=total_marks/num_students
+top_student=max(students,key=students.get)
+
+print('\n--- Student Performance Summary ---\n')
+
+for name,marks in students.items():
+    grade = calculate_grade(marks)
+    print(f"Name: {name}, Marks: {marks}, Grade: {grade}")
+
+print(f"\nAverage Marks: {average_marks:.2f}")
+print(f"Top Performer: {top_student} ({students[top_student]} marks)")
